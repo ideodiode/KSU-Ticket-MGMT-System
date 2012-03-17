5af1c0429ef50c013df97a44e6c79c6bc416ad1a
Ben McCrary <bensmcc@gmail.com>
2012-03-17 03:09:34 -0400

    added a header file so you don't have to do the style thing more than once

---
+++ application/views/includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+	<title>Sign Up!</title>
+	<link rel="stylesheet" href="<?php echo base_url();?>/css/style.css" type="text/css" media="screen" />
+	
+</head>
+<body>
