bfb0c2d317e593f9dc45d74d68f0b96dba1396cb
Ben McCrary <bensmcc@gmail.com>
2012-03-17 11:30:26 -0400

    Changed the title to be dynamic

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
-	<title>Sign Up!</title>
+	<title><?php echo $title; ?></title>
 	<link rel="stylesheet" href="<?php echo base_url();?>/css/style.css" type="text/css" media="screen" />
 	
 </head>
