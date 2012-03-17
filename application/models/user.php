91a24af9b6f42376e9f56a3dfe6427973db029d9
Ben McCrary <bensmcc@gmail.com>
2012-03-17 14:18:01 -0400

    added the create_user method that actually inserts the user into the db.

--- application/models/user.php
+++ application/models/user.php
@@ -1,14 +1,26 @@
 <?php
 
-class User extends CI_Model {
-	function validate() {
-		$this->db->where('email', $this->input->post('email'));
-		$this->db->where('password', sha1($this->input->post('password')));
-		$query = $this->db->get('users');
-		if ($query->num_rows == 1) {
-			return true;
+	class User extends CI_Model {
+		function validate() {
+			$this->db->where('email', $this->input->post('email'));
+			$this->db->where('password', sha1($this->input->post('password')));
+			$query = $this->db->get('users');
+			if ($query->num_rows == 1) {
+				return true;
+			}
 		}
-	}
 
-}
+		function create_user() {
+			$user_data = array(
+				'firstName' => $this->input->post('firstName'),
+				'lastName' => $this->input->post('lastName'),
+				'email' => $this->input->post('email'),
+				'password' => sha1($this->input->post('password')),
+				'role' => 'patron'
+			);
+			$insert = $this->db->insert('users', $user_data);
+			return $insert;
+		}
+
+	}
 ?>
