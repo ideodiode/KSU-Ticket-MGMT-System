ec0ce24c5a36c03ddaee9334318d284d3d8c2230
Bensmcc <bensmcc@gmail.com>
2012-03-22 17:06:53 -0400

    Separated the model/controller a bit more. Validate function takes two arguments.

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,26 +1,26 @@
 <?php
 
-	class User_model extends CI_Model {
-		function validate() {
-			$this->db->where('email', $this->input->post('email'));
-			$this->db->where('password', sha1($this->input->post('password')));
-			$query = $this->db->get('Users');
-			if ($query->num_rows == 1) {
-				return true;
-			}
+class User_model extends CI_Model {
+	function validate($email, $password) {
+		$this->db->where('email', $email);
+		$this->db->where('password', sha1($$password));
+		$query = $this->db->get('Users');
+		if ($query->num_rows == 1) {
+			return true;
 		}
-
-		function create_user($firstName, $lastName, $email, $password) {
-			$user_data = array(
-				'firstName' => $firstName,
-				'lastName' => $lastName,
-				'email' => $email,
-				'password' => sha1($password),
-				'role' => 'patron'
-			);
-			$insert = $this->db->insert('Users', $user_data);
-			return $insert;
-		}
-
 	}
+
+	function create_user($firstName, $lastName, $email, $password) {
+		$user_data = array(
+			'firstName' => $firstName,
+			'lastName' => $lastName,
+			'email' => $email,
+			'password' => sha1($password),
+			'role' => 'patron'
+		);
+		$insert = $this->db->insert('Users', $user_data);
+		return $insert;
+	}
+
+}
 ?>
