fdfb8540bfa2430306bdb506f8f6290374a47cf9
fab327 <spiritualsilver@gmail.com>
2012-03-27 13:06:43 -0400

    updated css
    still working on displaying within view

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,4 +1,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> <!-- Load jquery from google so caching is better -->
+<head>
+<link href="../../../css/style.css" rel="stylesheet" type="text/css" />
+</head>
 
 </body>
 
