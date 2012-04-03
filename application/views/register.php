9d8479210cee2332d2d0b6644905279216643674
Ben McCrary <Bensmcc@gmail.com>
2012-04-03 16:07:26 -0400

    Revert "styled registration page slightly"
    This reverts commit 28ef7b92519ed958aef8ac325b278efd47d9addd.
    
    Conflicts:
    
    	application/views/register.php
    	css/style.css

--- application/views/register.php
+++ application/views/register.php
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 <h1>Create an account</h1>
 <fieldset>
 	<legend>
@@ -6,49 +7,47 @@
 	</legend>
 	<?php
 		echo form_open('login/signup');
+=======
+<?php
+>>>>>>> parent of 28ef7b9... styled registration page slightly
 
-		echo form_input(array(
-			'name' => 'firstName',
-			'placeholder' => 'First Name',
-			'value' => set_value('firstName', '')
-		));
-		echo form_input(array(
-			'name' => 'lastName',
-			'placeholder' => 'Last Name',
-			'value' => set_value('lastName', '')
-		));
+echo form_open('login/signup');
 
-		echo form_input(array(
-			'name' => 'phoneNumber',
-			'placeholder' => 'Phone Number',
-			'maxLength' => '15',
-			'value' => set_value('phoneNumber', '')
-		));
-	?>
-</fieldset>
-<fieldset>
-	<legend>
-		Login information
-	</legend>
-	<?php
+echo form_input(array(
+	'name' => 'firstName',
+	'placeholder' => 'First Name',
+	'value' => set_value('firstName', '')
+));
+echo form_input(array(
+	'name' => 'lastName',
+	'placeholder' => 'Last Name',
+	'value' => set_value('lastName', '')
+));
 
-		echo form_input(array(
-			'name' => 'email',
-			'placeholder' => 'Email Address',
-			'value' => set_value('email', '')
-		));
-		echo form_password(array(
-			'name' => 'password',
-			'placeholder' => 'Password'
-		));
-		echo form_password(array(
-			'name' => 'password1',
-			'placeholder' => 'Confirm password'
-		));
+echo form_input(array(
+	'name' => 'email',
+	'placeholder' => 'Email Address',
+	'value' => set_value('email', '')
+));
+echo form_input(array(
+	'name' => 'phoneNumber',
+	'placeholder' => 'Phone Number',
+	'maxLength' => '15',
+	'value' => set_value('phoneNumber', '')
+));
+echo form_password(array(
+	'name' => 'password',
+	'placeholder' => 'Password'
+));
+echo form_password(array(
+	'name' => 'password1',
+	'placeholder' => 'Confirm password'
+));
 
-		echo form_submit('submit', 'Create Account');
-		echo form_close();
+echo form_submit('submit', 'Create Account');
+echo form_close();
 
+<<<<<<< HEAD
 		echo validation_errors();
 	?>
 </fieldset>
@@ -183,3 +182,6 @@
     
 </div>
 >>>>>>> upstream/master
+=======
+echo validation_errors();
+>>>>>>> parent of 28ef7b9... styled registration page slightly
