c03346c6261a57c88cdfb839560a441ab856858a
Ben McCrary <bensmcc@gmail.com>
2012-04-14 16:08:23 -0400

    Added a thing to make the rows highlight when you're hovering over them so that you can tell which row you're on. Not necessary, but it maybe looks nice?

--- application/views/tableView.php
+++ application/views/tableView.php
@@ -5,9 +5,7 @@
 	<thead>
 		<?php foreach($fields as $field_name => $field_display):
 		?>
-		<th <?php if ($sort_by == $field_name) echo 'class="sort_'.$sort_order.'"' ?>>
-			<?php echo anchor($role . "/" . $table . "_table/$field_name/" . (($sort_order == 'asc' && $sort_by == $field_name) ? 'desc' : 'asc'), $field_display);?>
-		</th>
+		<th <?php if ($sort_by == $field_name) echo 'class="sort_'.$sort_order.'"' ?>><?php echo anchor($role . "/" . $table . "_table/$field_name/" . (($sort_order == 'asc' && $sort_by == $field_name) ? 'desc' : 'asc'), $field_display);?></th>
 		<?php endforeach;?>
 	</thead>
 	<tbody>
@@ -30,8 +28,28 @@
 <?php endif;?>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="<?php echo base_url();?>/scripts/lightenDarken.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
-	$('tr:odd').css('background', '#e3e3e3');
-	$('tr:even').css('background', '#FEBC11');
+	// set the color for each row
+	var col1 = '#e3e3e3';
+	var col2 = '#FEBC11';
+	
+	//how much each row should change
+	var changeAmount = -25;
+	
+	// odd rows get one color, even rows get another
+	$('tr:odd').css('background', col1);	
+	$('tr:even').css('background', col2);
+
+	$('tr:odd').hover(function() {// when your mouse enters a row, darken the background. When it exits, restore the color
+		$(this).css('background', LightenDarkenColor(col1, changeAmount));
+	}, function() {
+		$(this).css('background', col1);
+	});
+	$('tr:even').hover(function() {
+		$(this).css('background', LightenDarkenColor(col2, changeAmount));
+	}, function() {
+		$(this).css('background', col2);
+	});
 
 </script>
