46345517ee19c0ca710bee54873b90481e33486d
Ben McCrary <bensmcc@gmail.com>
2012-04-21 05:08:42 -0400

    Finally got it working with one edit. Need to do more testing but the ajax is somwhat working at the moment. Yay!

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,66 +1,80 @@
 <?php
 
-class Admin extends Admin_Controller {
+	class Admin extends Admin_Controller {
 
-	function index() {
-		$this->load->model('user_model');
-		$data = array(
-			'main_content' => 'admin/index',
-			'title' => 'Admin Page',
-			'user' => $this->user_model->get_info($this->session->userdata('email'))
-		);
-		$this->load->view('includes/template', $data);
-	}
-
-	function update() {
-		$this->load->model('user_model');
-
-		$data = array(
-			'main_content' => 'admin/index',
-			'secondary_content' => 'update',
-			'user' => $this->user_model->get_info($this->session->userdata('email'))
-		);
-		$this->load->view('includes/template', $data);
-
-	}
-
-	function update_info() {
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules('firstName', 'First Name', 'trim|required');
-		$this->form_validation->set_rules('lastName', 'Last Name', 'trim|required');
-		$this->form_validation->set_rules('phone', 'Phone number', 'numeric');
-
-		if (!$this->form_validation->run()) {// if the validation fails
-		} else {
+		function index() {
 			$this->load->model('user_model');
-			if ($this->user_model->update_user($this->session->userdata('email'), $this->input->post('firstName'), $this->input->post('lastName'), $this->input->post('phone'))) {// if we successfully update the user information
-				$data = array(
-					'main_content' => 'tech/update',
-					'user' => $this->user_model->get_info($this->session->userdata('email')),
-					'message' => 'Successfully updated your info'
-				);
-				$this->load->view('includes/template', $data);
-			} else {
+			$data = array(
+				'main_content' => 'admin/index',
+				'title' => 'Admin Page',
+				'user' => $this->user_model->get_info($this->session->userdata('email'))
+			);
+			$this->load->view('includes/template', $data);
+		}
 
+		function update() {
+			$this->load->model('user_model');
+
+			$data = array(
+				'main_content' => 'admin/index',
+				'secondary_content' => 'update',
+				'user' => $this->user_model->get_info($this->session->userdata('email'))
+			);
+			$this->load->view('includes/template', $data);
+
+		}
+
+		function update_info() {
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules('firstName', 'First Name', 'trim|required');
+			$this->form_validation->set_rules('lastName', 'Last Name', 'trim|required');
+			$this->form_validation->set_rules('phone', 'Phone number', 'numeric');
+
+			if (!$this->form_validation->run()) {// if the validation fails
+			} else {
+				$this->load->model('user_model');
+				if ($this->user_model->update_user($this->session->userdata('email'), $this->input->post('firstName'), $this->input->post('lastName'), $this->input->post('phone'))) {// if we successfully update the user information
+					$data = array(
+						'main_content' => 'tech/update',
+						'user' => $this->user_model->get_info($this->session->userdata('email')),
+						'message' => 'Successfully updated your info'
+					);
+					$this->load->view('includes/template', $data);
+				} else {
+
+				}
 			}
+
+		}
+
+		function user_table($sort_by = 'user_id', $sort_order = 'asc', $offset = 0) {
+			$this->load->library('tablebuilder');
+			$this->tablebuilder->display($sort_by, $sort_order, $offset, 'admin', 'user', $this->session->userdata('id'));
+		}
+
+		function requests_table($sort_by = 'request_id', $sort_order = 'asc', $offset = 0) {
+			$this->load->library('tablebuilder');
+			$this->tablebuilder->display($sort_by, $sort_order, $offset, 'admin', 'requests', $this->session->userdata('id'));
+		}
+
+		function schedule_table($sort_by = 'user_id', $sort_order = 'asc', $offset = 0) {
+			$this->load->model('schedule');
+			$table_data = $this->schedule->get_schedule();
+
+			$content['main_content'] = 'admin/index';
+			$content['secondary_content'] = 'admin/schedule_table';
+			$content['table'] = $table_data;
+			$this->load->view('includes/template', $content);
+		}
+
+		function update_schedule() {
+			$this->load->model('schedule');
+			$user_id = $this->input->post('user_id');
+			$day_of_week = $this->input->post('day_of_week');
+			$when = $this->input->post('when');
+			$time = $this->input->post('time');
+			return $this->schedule->update_schedule($user_id, $day_of_week, $when, $time);
+
 		}
 
 	}
-
-
-	function user_table($sort_by = 'user_id', $sort_order = 'asc', $offset = 0) {
-		$this->load->library('tablebuilder');
-		$this->tablebuilder->display($sort_by, $sort_order, $offset, 'admin', 'user', $this->session->userdata('id'));
-	}
-	
-	function requests_table($sort_by = 'request_id', $sort_order = 'asc', $offset = 0) {
-		$this->load->library('tablebuilder');
-		$this->tablebuilder->display($sort_by, $sort_order, $offset, 'admin', 'requests', $this->session->userdata('id'));
-	}
-	function schedule_table($sort_by = 'user_id', $sort_order = 'asc', $offset = 0) {
-		$this->load->library('tablebuilder');
-		$this->tablebuilder->display($sort_by, $sort_order, $offset, 'admin', 'schedule', $this->session->userdata('id'));
-	}
-	
-	
-}
---
+++ application/views/admin/schedule_table.php
@@ -0,0 +1,106 @@
+<table>
+	<thead>
+		<tr>
+			<?php
+				foreach ($table['headings'] as $t) {
+					echo '<th>' . $t . '</th>';
+				}
+			?>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			foreach ($table['data'] as $row) {
+				$uid = $row['user'];
+				echo '<tr class="edit_r" id="' . $uid . '">';
+				foreach ($row as $day => $r) {
+					if (!is_array($r)) {
+						echo '<td><span class="user_id ' . $uid . '">' . $row['user'] . '</span>';
+						echo '<a class="save_schedule">Save</a>';
+					} else {
+						$start = date('g A', strtotime($r['start']));
+						$end = date('g A', strtotime($r['end']));
+						echo '<td><span class="row_' . $uid . '">' . $start . ' - ' . $end . '</span>';
+						echo '<div class="hide_input row_input_' . $uid . '">';
+						echo '<select data-user-id="' . $uid . '" data-day="' . $day . '" data-when="start" class="selector">';
+						foreach ($table['times'] as $t) {
+							if ($start == $t) {// start time
+								echo '<option selected="selected" value="' . $t . '">' . $t . '</option>';
+							} else {
+								echo '<option value="' . $t . '">' . $t . '</option>';
+							}
+
+						}
+						echo '</select>';
+
+						echo ' - ';
+						echo '<select data-user-id="' . $uid . '" data-day="' . $day . '" data-when="end" class="selector">';
+						foreach ($table['times'] as $t) {
+							if ($end == $t) {
+								echo '<option selected="selected" value="' . $t . '">' . $t . '</option>';
+							} else {
+								echo '<option value="' . $t . '">' . $t . '</option>';
+							}
+							// end time
+						}
+						echo '</select>';
+						echo '</div>';
+
+						echo '</td>';
+					}
+
+				}
+				echo '</tr>';
+			}
+		?>
+	</tbody>
+</table>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" charset="utf-8">
+	$('tr:odd').css('background-color', '#e3e3e3');
+	$('tr:even').css('background-color', '#FEBC11');
+
+	$('.edit_r').css('cursor', 'pointer');
+	$('.edit_r').click(function() {
+		var ID = $(this).attr('id');
+		$('.row_' + ID).hide();
+		$('.save_schedule').show();
+		$('.row_input_' + ID).show();
+		$('.save_schedule').css('display', 'block');
+	});
+
+	$('.selector').change(function() {
+		var updates = new Array();
+		var properties = new Array();
+
+		properties.push($(this).attr('data-user-id'));
+		properties.push($(this).attr('data-day'));
+		properties.push($(this).attr('data-when'));
+		properties.push($(this).val());
+		// [12, Tuesday, end, 8 AM]
+		var data = {
+			'user_id' : properties[0],
+			'day_of_week' : properties[1],
+			'when' : properties[2],
+			'time' : properties[3]
+		};
+
+		updates.push(data);
+
+		$('.save_schedule').click(function() {
+
+			$.ajax({
+				url: "<?php echo site_url('admin/update_schedule'); ?>",
+				data: updates[0],
+				type: 'POST',
+				success: function(msg) {
+					
+				}
+			});
+		});
+
+	});
+
+
+</script>
