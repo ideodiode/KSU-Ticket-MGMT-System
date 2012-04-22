e6aebbba61337420bbc9a47c677d73fc952d6633
Ben McCrary <Bensmcc@gmail.com>
2012-04-22 18:38:49 -0400

    Added all the FAQ stuff. Admin can add/delete items from it and everyone can view it.

--- application/controllers/faq.php
+++ application/controllers/faq.php
@@ -1,29 +1,14 @@
 <?php
 
-class Faq extends CI_Controller 
-{
-	
-	function add_question() 
-	{	
-		$this->load->helper(array('form', 'url'));
-		echo "success";
+class Faq extends CI_Controller {
+	function index() {
+		$this->load->model('faq_model');
+		$faqs = $this->faq_model->get_faqs();
+		$data = array(
+			'main_content' => 'faq',
+			'faqs' => $faqs
+		);
+		$this->load->view('includes/template', $data);
+	}
 
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules('question', 'Question', 'required');
-		$this->form_validation->set_rules('answer', 'Answer', 'required');
-
-		if ($this->form_validation->run() == FALSE)
-		{
-			$this->load->view('faq_form');
-		}
-		else
-		{
-			$this->load->model('faq_model');
-			$this->faq_model->insert_question();
-			$this->load->view('faq_form');
-		}
-	}	
-	
 }
-
-?>
